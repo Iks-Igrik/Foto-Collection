a422d9a66a20d9e654a06440cc0641337817294e
Iks-Igrik <maksnew@gmail.com>
2022-09-08 21:14:49 +0200

    Search by name, Pagination, Sorting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,69 @@
 import React from 'react';
 import './index.scss';
+import {Collection} from './Collection';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const cats = [
+  { "name": "All" },
+  { "name": "Sea" },
+  { "name": "Mountains" },
+  { "name": "Architecture" },
+  { "name": "City" }
+]
 
 function App() {
+
+const [categoryId, setCategoryId] = React.useState(0);
+const [page, setPage] = React.useState(1);
+const [isLoading, setIsLoading] = React.useState(true);
+const [searchValue, setSearchValue] = React.useState('');
+const [collections, setCollection] = React.useState([]);
+
+React.useEffect(() => {
+  setIsLoading(true);
+  const category = categoryId ? `categoryId=${categoryId}` : '';
+  fetch(`https://631a261c8e51a64d2bf73b8b.mockapi.io/NatureFoto?page=${page}&limit=3&${category}`)
+  .then((res) => res.json())
+  .then((json) => {
+    setCollection(json);
+  }).catch((err) => {
+    console.warn(err);
+    alert('Error data');
+  }).finally(() => setIsLoading(false))
+},[categoryId, page]);  
   return (
     <div className="App">
-      <h1>Моя коллекция фотографий</h1>
+      <h1>My photo collection</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+         {cats.map((obj, index) => <li 
+         onClick={() => setCategoryId(index)}
+         className={categoryId === index ? 'active' : ''}
+         key={obj.name}> {obj.name} </li>)}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input 
+        value={searchValue} 
+        onChange={e => setSearchValue(e.target.value)} 
+        className="search-input" 
+        placeholder="Search by name" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+       {isLoading ? (<h2>Loading...</h2>) :
+       (
+        collections.filter((obj) => obj.name.toLowerCase().includes(searchValue.toLocaleLowerCase())
+        ).map((obj, index) => (
+         <Collection
+          key={index}
+          name={obj.name}
+          images={obj.photos}
         />
+        ))
+       )} 
+               
       </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {[...Array(5)].map((_, i) => (
+          <li onClick={() => setPage(i + 1)} className={page === i + 1 ? 'active' : ''}>{i + 1}</li>
+        )) }
       </ul>
     </div>
   );
